9d1914d2a369f1070de2c68e155da1344aad0139
nKsnC <nksnc@qq.com>
2021-03-19 17:08:31 +0800

    feat: 文件上传中文支持

--- src/components/UploadInput.vue
+++ src/components/UploadInput.vue
@@ -16,20 +16,22 @@ import "filepond/dist/filepond.min.css";
 // Create component
 const FilePond = vueFilePond(FilePondPluginFileValidateType);
 
+// 语言设置
+import zh_CN from "filepond/locale/zh-cn.js";
+setOptions(zh_CN);
+
 // FilePond 全局设置
 setOptions({
-  labelIdle: "拖放文件到此",
   server: "/api/uploadfile",
   instantUpload: false,
   // 插件设置
   acceptedFileTypes:
     "application/vnd.android.package-archive, application/iphone",
-  labelFileTypeNotAllowed: "文件类型不支持",
+
   fileValidateTypeLabelExpectedTypesMap: {
     "application/vnd.android.package-archive": ".apk",
     "application/iphone": ".ipa",
   },
-  fileValidateTypeLabelExpectedTypes: "只支持 {allButLastType} 或 {lastType}",
 });
 
 // TODO 上传文件，服务器验证返回信息
