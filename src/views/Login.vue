61a49ab33fa6babd968a5eff93325aeace89a528
nKsnC <nksnc@qq.com>
2021-04-16 15:16:24 +0800

    feat: 登录与页面访问

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-input v-model="username" messages="用户名"></v-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
