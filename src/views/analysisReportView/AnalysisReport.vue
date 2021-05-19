9dbf5df2e238e73bd852950d10bf8ee080e43374
nKsnC <nksnc@qq.com>
2021-05-19 08:46:56 +0800

    feat: 添加分析报告页

---
+++ src/views/analysisReportView/AnalysisReport.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-sheet>
+      <div class=".text-heading3">MASAP 分析报告</div>
+      <div class=".text-heading4">{{ app.name }}</div>
+
+      <v-divider></v-divider>
+
+      <div>报告信息</div>
+
+      <v-divider></v-divider>
+
+      <div>应用基础信息</div>
+
+      <v-divider></v-divider>
+
+      <div>漏洞信息</div>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
